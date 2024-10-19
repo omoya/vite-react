634abbfd301fdf2bca0ff328497f49e0c80059b1
Oscar Moya <oscarmoyamesa@gmail.com>
2024-10-19 20:25:18 +0200

    chore: use App as parent component

--- 001-mytest/src/main.jsx
+++ 001-mytest/src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Car from "./Car.jsx";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Car />a
+    <App />
   </StrictMode>
 );
