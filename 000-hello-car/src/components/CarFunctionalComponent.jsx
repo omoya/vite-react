98d75014340694342b3c89fa369cff8c3bf835d8
Oscar Moya <oscarmoyamesa@gmail.com>
2024-10-20 10:28:34 +0200

    chore: be more precise

--- 000-hello-car/src/components/CarFunctionalComponent.jsx
+++ 000-hello-car/src/components/CarFunctionalComponent.jsx
@@ -1,3 +1,5 @@
 export default function CarFunctionalComponent() {
-  return <h2>Hi, I am a functional Car! Easier, don't you think?</h2>;
+  return (
+    <h2>Hi, I am a functional Car! Already a bit simpler, don't you think?</h2>
+  );
 }
