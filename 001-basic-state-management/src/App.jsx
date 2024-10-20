0b92bc85facd56e4c96b42bbfa70eb8965b5d008
Oscar Moya <oscarmoyamesa@gmail.com>
2024-10-20 10:55:57 +0200

    chore: remove an unnecesary car... walking is often a good choice

--- 001-basic-state-management/src/App.jsx
+++ 001-basic-state-management/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Car from "./components/Car.jsx";
 import UserClassComponent from "./components/UserClassComponent.jsx";
 import UserFunctionalComponent from "./components/UserFunctionalComponent.jsx";
 
 function App() {
   return (
     <>
-      <Car></Car>
       <UserClassComponent
         initialName="John"
         initialAge={25}
